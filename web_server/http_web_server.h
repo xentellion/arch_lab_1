cfac0a5075e33641ac368e9f1fafdee2d42ff404
Xent <xentellion@xentellion-bots.ru>
2022-04-14 15:05:41 +0300

    port change

--- web_server/http_web_server.h
+++ web_server/http_web_server.h
@@ -170,7 +170,7 @@ protected:
         {
             unsigned short port = (unsigned short)
                                       config()
-                                          .getInt("HTTPWebServer.port", 8080);
+                                          .getInt("HTTPWebServer.port", 80);
             std::string format(
                 config().getString("HTTPWebServer.format",
                                    DateTimeFormat::SORTABLE_FORMAT));
